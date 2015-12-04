1a961f8a4c5871c163ee62d4410a3bc7141e3e24
cangjie <cangjie@hotmail.com>
2015-12-04 18:03:20 +0800

    sync some functions

--- App_Code/Util.cs
+++ App_Code/Util.cs
@@ -192,6 +192,13 @@ public class Util
 
     }
 
+
+    public static string GetLongTimeStamp(DateTime currentDateTime)
+    {
+        TimeSpan ts = currentDateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+    }
+
     public static string Get2014SummerCampImageForWexinNews()
     {
         string xmlStr = "<item>"
