1ba5621fc49e27006ab71f0e073b55654c14fdbe
cangjie <cangjie@hotmail.com>
2015-12-09 16:01:12 +0800

    view submit data

--- App_Code/RepliedMessage.cs
+++ App_Code/RepliedMessage.cs
@@ -100,7 +100,7 @@ public class RepliedMessage
         object v;
         json.TryGetValue("errcode", out v);
         if (!v.ToString().Trim().Equals("0"))
-            throw new Exception(str);
+            throw new Exception(jsonFormatData+"\r\n"+str);
         return int.Parse(v.ToString());
     }
 
