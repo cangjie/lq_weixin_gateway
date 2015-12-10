640ac3d5c27deecd990a3fee481d1a9581b701e2
cangjie <cangjie@hotmail.com>
2015-12-10 15:46:38 +0800

    padleft

--- App_Code/DealMessage.cs
+++ App_Code/DealMessage.cs
@@ -311,7 +311,7 @@ public class DealMessage
                 break;
             case "4":
                 GroupMaster groupMaster = GroupMaster.CreateNew(repliedMessage.to.Trim());
-                string randGroupCode = "K"+groupMaster.ID.ToString();
+                string randGroupCode = "K"+groupMaster.ID.ToString().PadLeft(4,'0');
                 repliedMessage.type = "text";
                 repliedMessage.content = "把邀请码【" + randGroupCode + "】发放到群里，让群成员关注“卢勤问答平台”，并在输入框里输入邀请码【" + randGroupCode + "】，点击弹出的文章里面的同意。当同意人数超过300人，请将同意人数截图给平台小助手，然后由平台小助手安排授课。";
                 break;
