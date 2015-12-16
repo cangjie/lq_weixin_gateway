d84f3b73e83f070d00e1c712b5744a4e37df07a8
cangjie <cangjie@hotmail.com>
2015-12-16 12:28:44 +0800

    cancel draw

--- App_Code/DealMessage.cs
+++ App_Code/DealMessage.cs
@@ -169,7 +169,7 @@ public class DealMessage
                 }
                 if (!deal)
                 {
-                    RepliedMessage.news[] newsArr = new RepliedMessage.news[4];
+                    RepliedMessage.news[] newsArr = new RepliedMessage.news[3];
 
                     newsArr[0] = new RepliedMessage.news();
                     newsArr[0].picUrl = "http://weixin.luqinwenda.com/dingyue/images/1.jpg";
@@ -177,23 +177,18 @@ public class DealMessage
                     newsArr[0].description = "欢迎关注卢勤问答";
                     newsArr[0].url = "http://mp.weixin.qq.com/s?__biz=MzA3MTM1OTIwNg==&mid=201403673&idx=1&sn=1ba5e81f3d925935117caf8abb12ac84#rd";
 
+                  
                     newsArr[1] = new RepliedMessage.news();
-                    newsArr[1].title = "文明小使者畅游新加坡——2016新加坡精品冬令营";
-                    newsArr[1].description = "文明小使者畅游新加坡——2016新加坡精品冬令营";
-                    newsArr[1].url = "http://mp.weixin.qq.com/s?__biz=MzA3MTM1OTIwNg==&mid=400281989&idx=1&sn=c309e1c612690003cc6d15bd64f379c9#rd";
-                    newsArr[1].picUrl = "http://weixin.luqinwenda.com/images/singapore_2016_s.jpg";
-
-                    newsArr[2] = new RepliedMessage.news();
-                    newsArr[2].picUrl = "http://weixin.luqinwenda.com/images/talk.jpg";
-                    newsArr[2].title = "“我要学演说” 少年口才培训火热征募中……";
-                    newsArr[2].description = "“我要学演说” 少年口才培训火热征募中……";
-                    newsArr[2].url = "http://mp.weixin.qq.com/s?__biz=MzA3MTM1OTIwNg==&mid=213625859&idx=1&sn=b1707b0fcc208232951b36fe66bc4e33#rd";
+                    newsArr[1].picUrl = "http://weixin.luqinwenda.com/images/talk.jpg";
+                    newsArr[1].title = "“我要学演说” 少年口才培训火热征募中……";
+                    newsArr[1].description = "“我要学演说” 少年口才培训火热征募中……";
+                    newsArr[1].url = "http://mp.weixin.qq.com/s?__biz=MzA3MTM1OTIwNg==&mid=213625859&idx=1&sn=b1707b0fcc208232951b36fe66bc4e33#rd";
                     
-                    newsArr[3] = new RepliedMessage.news();
-                    newsArr[3].picUrl = "http://weixin.luqinwenda.com/images/weiketang.jpg";
-                    newsArr[3].title = "报名启动：卢勤公益微课堂 开讲了";
-                    newsArr[3].description = "报名启动：卢勤公益微课堂 开讲了";
-                    newsArr[3].url = "http://mp.weixin.qq.com/s?__biz=MzA3MTM1OTIwNg==&mid=401732145&idx=2&sn=a27d4b3a904e26c11e47c5f095e4bd01&scene=1&srcid=1127GL8FPbhHEDtwNljbbHOX&key=ff7411024a07f3eb0913a730e8e5ce7baee0049c9eaaeb2ca74ff9c09cc74e4fbafb3dbc59d34431b7b9774da5ae79c2&ascene=0&uin=ODk3MzEzNzY0&devicetype=iMac+MacBookAir6%2C2+OSX+OSX+10.11.1+build(15B42)&version=11020201&pass_ticket=CV%2BXqWh4PB6EpCb%2BekSQtuF6X8nlpfhXEdvNC0vPwnJj4vTXD%2F1FZ3pTgin1zsZH";
+                    newsArr[2] = new RepliedMessage.news();
+                    newsArr[2].picUrl = "http://weixin.luqinwenda.com/images/weiketang.jpg";
+                    newsArr[2].title = "报名启动：卢勤公益微课堂 开讲了";
+                    newsArr[2].description = "报名启动：卢勤公益微课堂 开讲了";
+                    newsArr[2].url = "http://mp.weixin.qq.com/s?__biz=MzA3MTM1OTIwNg==&mid=401732145&idx=2&sn=a27d4b3a904e26c11e47c5f095e4bd01&scene=1&srcid=1127GL8FPbhHEDtwNljbbHOX&key=ff7411024a07f3eb0913a730e8e5ce7baee0049c9eaaeb2ca74ff9c09cc74e4fbafb3dbc59d34431b7b9774da5ae79c2&ascene=0&uin=ODk3MzEzNzY0&devicetype=iMac+MacBookAir6%2C2+OSX+OSX+10.11.1+build(15B42)&version=11020201&pass_ticket=CV%2BXqWh4PB6EpCb%2BekSQtuF6X8nlpfhXEdvNC0vPwnJj4vTXD%2F1FZ3pTgin1zsZH";
                     
 
                     repliedMessage.newsContent = newsArr;
@@ -393,10 +388,10 @@ public class DealMessage
                 int drawId = Drawing.DrawingPlay(receivedMessage.from.Trim(), actId);
                 repliedMessage.type = "news";
                 RepliedMessage.news drawing = new RepliedMessage.news();
-                drawing.title = "“卢勤微课堂”幸运抽奖活动";
+                drawing.title = "“卢勤微课堂”幸运抽奖活动已经结束，点击进入商城享受更多优惠！";
                 drawing.picUrl = "http://game.luqinwenda.com/images/draw_banner.jpg";
-                drawing.url = "http://game.luqinwenda.com/weiketang/LuckDraw.aspx?id=" + drawId.ToString() +"&openid=" + receivedMessage.from.Trim();
-                drawing.description = "“卢勤微课堂”幸运抽奖活动";
+                drawing.url = "http://mall.luqinwenda.com/index.aspx" ;
+                drawing.description = "“卢勤微课堂”幸运抽奖活动已经结束，点击进入商城享受更多优惠！";
                 repliedMessage.newsContent = new RepliedMessage.news[] { drawing };
                 break;
             case "平安":
