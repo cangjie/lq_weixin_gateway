4060afc37375560460eec8b58c482da6499886ed
cangjie <cangjie@hotmail.com>
2015-12-18 10:39:02 +0800

    set shorter token time

--- App_Code/Util.cs
+++ App_Code/Util.cs
@@ -178,7 +178,7 @@ public class Util
     public static string GetToken()
     {
         DateTime nowDate = DateTime.Now;
-        if (nowDate - tokenTime > new TimeSpan(2, 30, 0))
+        if (nowDate - tokenTime > new TimeSpan(0, 30, 0))
         {
             token = ForceGetToken();
         }
