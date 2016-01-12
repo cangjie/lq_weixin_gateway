7c2ed74114a1a75d52601c6ebb6878021f8902d1
cangjie <cangjie@hotmail.com>
2016-01-12 11:10:16 +0800

    drawing once

--- App_Code/Drawing.cs
+++ App_Code/Drawing.cs
@@ -21,7 +21,7 @@ public class Drawing
     {
         DataTable dt = DBHelper.GetDataTable(" select * from random_awards where open_id = '" + opneId.Trim() + "'  and act_id = " + actId.ToString(), Util.ConnectionStringGame);
         int i = 0;
-        if (dt.Rows.Count > 1)
+        if (dt.Rows.Count > 0)
         {
             i = int.Parse(dt.Rows[0]["id"].ToString());
         }
@@ -33,6 +33,51 @@ public class Drawing
         return i;
     }
 
+    public static int NewDrawing(string openId, int actId)
+    {
+        string award = "";
+        int seed = (new Random()).Next(0, 100);
+        if (seed < 20)
+        {
+            Coupon coupon = Coupon.AddCoupon(500);
+            award = "5元优惠券:" + coupon._fields["code"].ToString().Trim();
+        }
+        else
+        {
+            if (seed < 50)
+            {
+                Coupon coupon = Coupon.AddCoupon(1000);
+                award = "10元优惠券:" + coupon._fields["code"].ToString().Trim();
+            }
+            else
+            {
+                Coupon coupon = Coupon.AddCoupon(1500);
+                award = "15元优惠券:" + coupon._fields["code"].ToString().Trim();
+            }
+        }
+
+        string[,] insertParameter = {{"act_id", "int", actId.ToString()},
+                                    {"open_id", "varchar", openId.Trim()},
+                                    {"seed", "int", seed.ToString()},
+                                    {"award", "varchar", award.Trim()}};
+
+        int i = DBHelper.InsertData("random_awards", insertParameter, Util.ConnectionStringGame);
+
+        if (i == 1)
+        {
+            DataTable dt = DBHelper.GetDataTable(" select top 1 * from random_awards order by [id] desc ", Util.ConnectionStringGame);
+            if (dt.Rows.Count == 1)
+            {
+                i = int.Parse(dt.Rows[0]["id"].ToString());
+            }
+            dt.Dispose();
+        }
+
+        return i;
+    }
+
+
+    /*
     public static int NewDrawing(string opneId, int actId)
     {
         int numFannao = 0;
@@ -164,5 +209,5 @@ public class Drawing
         return i;
     }
 
-
+    */
 }
